a8007e8ffb79a0f260632a32a3b0c9b1c013fca4
Brain <poc008@ucsd.edu>
2025-10-22 23:16:52 -0700

    Add files via upload

---
+++ swap_sum.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def swap_sum(A, B):
+    SA = sum(A)
+    SB = sum(B)
+    diff = SB - SA - 10
+
+    if diff % 2 != 0:
+        return None
+    delta = diff // 2  # 目標是找 b - a == delta
+
+    i = 0
+    j = 0
+    nA = len(A)
+    nB = len(B)
+
+    while i < nA and j < nB:
+        cur = B[j] - A[i]
+        if cur == delta:
+            return (i, j)
+        elif cur < delta:
+            j += 1
+        else:
+            i += 1
+
+    return None
+
+
+# In[ ]:
+
+
+
+
