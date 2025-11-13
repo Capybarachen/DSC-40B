1b174929d1288255b3acdfac137c64b826d158d5
Brain <poc008@ucsd.edu>
2025-11-12 21:45:42 -0800

    Add files via upload

---
+++ assign_good_and_evil.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+def assign_good_and_evil(graph):
+    labels = {}
+
+    for start in graph.nodes():
+        if start in labels:
+            continue
+
+        labels[start] = 'good'
+        queue = [start]
+
+        while len(queue) > 0:
+            current = queue[0]
+            del queue[0]
+
+            for neighbor in graph.neighbors(current):
+                if neighbor not in labels:
+                    if labels[current] == 'good':
+                        labels[neighbor] = 'evil'
+                    else:
+                        labels[neighbor] = 'good'
+                    queue.append(neighbor)
+                else:
+                    if labels[neighbor] == labels[current]:
+                        return None
+
+    return labels
+
+
+# In[ ]:
+
+
+
+
