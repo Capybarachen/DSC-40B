621875eb6a98f1d92e9eb79f0ba4b678fd9f8163
Brain <poc008@ucsd.edu>
2025-12-11 22:31:25 -0800

    Update min_ell_theta.py

--- min_ell_theta.py
+++ min_ell_theta.py
@@ -5,52 +5,47 @@
 
 
 def learn_theta(data, colors):
-    max_blue = float('-inf')
-    min_red = float('inf')
-
-    for x, c in zip(data, colors):
-        if c == 'blue':
-            if x > max_blue:
-                max_blue = x
-        else:  # red
-            if x < min_red:
-                min_red = x
-
-    return 0.5 * (max_blue + min_red)
+    max_blue = None
+    for i in range(len(data)):
+        if colors[i] == 'blue':
+            if max_blue is None or data[i] > max_blue:
+                max_blue = data[i]
+    return max_blue
 
 
 def compute_ell(data, colors, theta):
     loss = 0
-
-    for x, c in zip(data, colors):
-        if c == 'red' and x <= theta:
-            loss += 1
-        elif c == 'blue' and x > theta:
-            loss += 1
-
+    for i in range(len(data)):
+        x = data[i]
+        c = colors[i]
+        if c == 'red':
+            if x <= theta:
+                loss = loss + 1
+        else:
+            if x > theta:
+                loss = loss + 1
     return float(loss)
 
 
 def minimize_ell(data, colors):
     best_theta = data[0]
     best_loss = compute_ell(data, colors, best_theta)
-
-    for theta in data:
+    for i in range(len(data)):
+        theta = data[i]
         loss = compute_ell(data, colors, theta)
         if loss < best_loss:
             best_loss = loss
             best_theta = theta
-
-    return best_theta
+    return float(best_theta)
 
 
 def minimize_ell_sorted(data, colors):
     n = len(data)
 
     total_blue = 0
-    for c in colors:
-        if c == 'blue':
-            total_blue += 1
+    for i in range(n):
+        if colors[i] == 'blue':
+            total_blue = total_blue + 1
 
     red_leq_theta = 0
     blue_gt_theta = total_blue
@@ -60,19 +55,15 @@ def minimize_ell_sorted(data, colors):
 
     for alpha in range(1, n + 1):
         if colors[alpha - 1] == 'red':
-            red_leq_theta += 1
+            red_leq_theta = red_leq_theta + 1
         else:
-            blue_gt_theta -= 1
+            blue_gt_theta = blue_gt_theta - 1
 
-        if alpha < n:
-            theta = 0.5 * (data[alpha - 1] + data[alpha])
-        else:
-            theta = data[-1] + 1.0
-
+        theta = data[alpha - 1]
         loss = red_leq_theta + blue_gt_theta
+
         if loss < best_loss:
             best_loss = loss
             best_theta = theta
 
-    return best_theta
-
+    return float(best_theta)
